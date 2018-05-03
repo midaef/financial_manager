0e71b3d8d7cace6eeb9c7fd95a0b3e213fcaa896
Dmitriy Stepanov <stdian@mail.ru>
2018-05-03 22:54:39 +0800

    Added login and fixed register. Fixed creating db table. Changed work with db. Added comments for DanilPresent.

--- main.py
+++ main.py
@@ -4,52 +4,84 @@ import os
 import sqlite3
 
 
-db_host = 'localhost'
-db_username = 'root'
-db_password = ''
-db_db = 'hash'
-db_charset = 'utf8'
-db_table = 'users'
 db_filename = '1.db'
-base_dir = 'base'
 
+session = []
+connection = ''
+cursor = ''
 
 def connect_db():
-	global c
-	try:
-		connection = sqlite3.connect(db_filename)
-		c = connection.cursor()
-	except:
-		print('Connect ERROR!\nCheck internet connection')
-		exit()
+	global connection
+	global cursor
+	connection = sqlite3.connect('database.db')
 
 	
 def create_table():
 	connect_db()
-	c.execute('CREATE TABLE IF NOT EXISTS users ( login TEXT NOT NULL , password VARCHAR(32) NOT NULL )')
-	c.execute('CREATE TABLE IF NOT EXISTS data ( id INT(11) NOT NULL AUTO_INCREMENT , login VARCHAR(64) NOT NULL , type INT(1) NOT NULL , date DATE NOT NULL , money INT(11) NOT NULL , PRIMARY KEY (id))')
-	c.close()
+	cursor = connection.cursor()
+	cursor.execute('CREATE TABLE IF NOT EXISTS users ( login TEXT NOT NULL , password VARCHAR(32) NOT NULL )')
+	cursor.execute('CREATE TABLE IF NOT EXISTS data ( id INTEGER(11) PRIMARY KEY NOT NULL , login VARCHAR(64) NOT NULL , type INT(1) NOT NULL , date DATE NOT NULL , money INT(11) NOT NULL )')
+	cursor.close()
 
 
 def main():
-	p('1. Login')
-	p('2. Register')
-	v = input('Input value: ')
-	if v == '1':
-		l2 = input('Input login: ')
-		p2 = input('Input password: ')
-	if v == '2':
-		l = input('Input new login: ')
-		p = input('Input new password: ')
-		try:
-			query = "INSERT INTO users (login, password) VALUES ('" + l + "', '" + p + "')"
-			args = (password, hash)
-			cursor.execute(query)
-			cursor.close()
-		except:
-			cursor.close()
+	global session
+
+	while True:
+		if session == []:
+			p('1. Login')
+			p('2. Register')
+			choose = input('Input value: ')
+			if choose == '1':
+				login = input('Input login: ')
+				passwd = input('Input password: ')
+
+				cursor = connection.cursor()
+
+				query = "SELECT password FROM users WHERE login = " + '"' + login + '"'
+				cursor.execute(query)
+				data = cursor.fetchall()
+				if data == []:
+					p('Wrong login or password')
+					return
+				
+				password = data[0][0]
+
+				if passwd == password:
+					session = [login, password]
+					p('Login successful')
+				else:
+					p('Wrong login or password')
+
+			if choose == '2':
+				login = input('Input new login: ')
+				passwd = input('Input new password: ')
+
+				cursor = connection.cursor()
+
+				query = "SELECT * FROM users WHERE login = " + '"' + login + '"'
+				cursor.execute(query)
+				data = cursor.fetchall()
+				if data == []:
+					query = "INSERT INTO users (login, password) VALUES ('" + login + "', '" + passwd + "')"
+					cursor.execute(query)
+					cursor.close()
+					connection.commit()
+				else:
+					p('Login is busy')
+					cursor.close()
+			else:
+				break
+				# тут будут комадны
 
 
 if __name__ == '__main__':
 	create_table()
 	main()
+
+
+
+
+# перед действием с бд надо создать курсор      cursor = connection.cursor()
+# если были изменения в бд надо сделать коммит  connection.commit()
+# после действия закрыть                        cursor.close()
